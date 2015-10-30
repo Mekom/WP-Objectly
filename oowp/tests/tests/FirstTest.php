0be3a5482b249347a4f7a1bd8eb26562cc4088ca
sigurdsvela <sigurdbergsvela@gmail.com>
2015-10-30 21:41:03 +0100

    Fix first test to success

--- oowp/tests/tests/FirstTest.php
+++ oowp/tests/tests/FirstTest.php
@@ -3,6 +3,6 @@ namespace oowp\tests\tests;
 
 class FirstTest extends \WP_UnitTestCase {
     public function testTest() {
-        $this->assertTrue(false, "This failed");
+        $this->assertTrue(true, "This succeeds");
     }
 }
