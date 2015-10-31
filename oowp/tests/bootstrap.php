b4c5c3c1c19273c091c33797929dda1e3551d26a
sigurdsvela <sigurdbergsvela@gmail.com>
2015-10-31 15:05:36 +0100

    Remove debug lines in phpunit bootstrap.php

--- oowp/tests/bootstrap.php
+++ oowp/tests/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
-var_dump(__FILE__ . ":" . __LINE__);
 require_once __DIR__ . "/../../vendor/autoload.php";
-var_dump(__FILE__ . ":" . __LINE__);
 require_once __DIR__ . "/WP-CoreTestLib/bootstrap.php";
 require_once __DIR__ . "/../../autoload.php";
