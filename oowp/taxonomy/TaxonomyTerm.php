afcb5ca4f0d5e577d83eec04b16458224d3bfaf1
sigurdsvela <sigurdbergsvela@gmail.com>
2015-11-01 15:24:18 +0100

    Remove static method in ITaxonomyTerm that are implemented
    These methods where being implemented in the TaxonomyTerm class anyway.
    No need to list them in this interface.

--- oowp/taxonomy/TaxonomyTerm.php
+++ oowp/taxonomy/TaxonomyTerm.php
@@ -8,20 +8,6 @@ use InvalidArgumentException;
 /* Abstract static methods */
 interface ITaxonomyTerm {
     /**
-     * Get a taxonomy term from a term id
-     *
-     * @return static The term
-     */
-    static function fromTermID($termID);
-
-    /**
-     * Get a taxonomy term from a term slug
-     *
-     * @return static The term
-     */
-    static function fromTermSlug($termSlug);
-
-    /**
      * Get the taxonomy for this term
      *
      * @return Taxonomy
