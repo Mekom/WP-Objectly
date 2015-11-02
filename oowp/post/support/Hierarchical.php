6fd5fe92549df11b6c22d72d15552700badb3e3b
sigurdsvela <sigurdbergsvela@gmail.com>
2015-11-02 23:44:17 +0100

    Fix getPostParent for when no parent is set

--- oowp/post/support/Hierarchical.php
+++ oowp/post/support/Hierarchical.php
@@ -6,10 +6,12 @@ use oowp\post\PostObject;
 trait Hierarchical {
     /**
      * Get the parent post of this post
-     * @return PostObject The post parent
+     *
+     * @return PostObject|null The post parent, or null if this post has no parent
      */
     public final function getPostParent() {
         $parentID = wp_get_post_parent_id($postID);
+        if ($parentID === false) return null;
         return static::fromPostID($parentID);
     }
 
