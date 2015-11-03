b13f2af95e2ff7176ae3823f01096f8a8642ba7e
sigurdsvela <sigurdbergsvela@gmail.com>
2015-11-03 03:25:33 +0100

    Implement CorePostObject class

--- oowp/post/CorePostObject.php
+++ oowp/post/CorePostObject.php
@@ -1,9 +1,29 @@
 <?php
+namespace oowp\post;
+
+use oowp\post\support\Author;
+use oowp\post\support\IAuthor;
+use oowp\post\support\Excerpt;
+use oowp\post\support\IExcerpt;
+use oowp\post\support\Content;
+use oowp\post\support\IContent;
+use oowp\post\support\CustomFields;
+use oowp\post\support\ICustomFields;
+use oowp\post\support\Title;
+use oowp\post\support\ITitle;
+use oowp\post\support\Slug;
+use oowp\post\support\ISlug;
 
 /**
  * Represents one of the core post objects
  * post, page, attachment, and revision.
  */
-class CorePostObject extends PostObject {
-
+class CorePostObject extends PostObject implements
+    IAuthor, IExcerpt, IContent, ICustomFields, ISlug, ITitle {
+    use Author;
+    use Excerpt;
+    use Content;
+    use CustomFields;
+    use Slug;
+    use Title;
 }
