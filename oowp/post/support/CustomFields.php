d2480fc3b135ff787746ff1dcad578eab6f9e124
sigurdsvela <sigurdbergsvela@gmail.com>
2015-11-06 02:47:34 +0100

    implement remove and has custom field in CustomFields

--- oowp/post/support/CustomFields.php
+++ oowp/post/support/CustomFields.php
@@ -25,4 +25,26 @@ trait CustomFields {
     public final function setPostMeta($key, $value) {
         update_post_meta($this->getPostID(), $key, $value);
     }
+
+    /**
+     * Remove a post meta
+     *
+     * @param string $key The key
+     * @return void
+     */
+    public final function deletePostMeta($key) {
+        delete_post_meta($this->getPostID(), $key);
+    }
+
+    /**
+     * Check if this post has a spesific post meta.
+     * This is simply a wrapper function that compares
+     * the result of getPostMeta($key) with empty string.
+     *
+     * @param string $key The key
+     * @return bool true if yes, false if no
+     */
+    public final function hasPostMeta($key) {
+        return $this->getPostMeta($key) !== "";
+    }
 }
